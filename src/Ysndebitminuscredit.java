a18f35ad30dc0465e078bd2bcb42b9e7f63f783d
ivanovroman4999-pixel <ivanovroman4999@gmail.com>
2025-11-12 12:08:35 +0300

    Delete src directory

--- src/Ysndebitminuscredit.java
+++
@@ -1,9 +0,0 @@
-import taxes.TaxSystem;
-
-public class Ysndebitminuscredit extends TaxSystem {
-    @Override
-    public int calcTaxFor(int debit, int credit){
-        int taxAmount = (int) ((debit - credit) * 0.15);
-        return Math.max(0, taxAmount);
-    }
-}
