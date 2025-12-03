6a01dffe6dbbfe75650a67cb04dd469ae965a92c
ivanovroman4999-pixel <ivanovroman4999@gmail.com>
2025-12-03 17:19:27 +0300

    Update DeliveryService.java

--- DeliveryService.java
+++ DeliveryService.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class DeliveryService {
     Map<Address, Integer> costPerAddress = new HashMap<>();
+    ArrayList list = new ArrayList();
     int totalDeliverySum = 0;
+    int totalAddress = 0;
+
     public boolean processOrder(String country, String city, int weight) {
         Address orderAddress = new Address(country, city); // Создаем объект Address для заказа
         
@@ -11,9 +15,14 @@ public class DeliveryService {
             int costPerKg = costPerAddress.get(orderAddress);
             int orderCost = costPerKg * weight;
             totalDeliverySum += orderCost; // Добавляем к общей сумме
-            
+            list.add(orderAddress);
             System.out.println("Стоимость доставки составит: " + orderCost + " руб");
             System.out.println("Общая стоимость всех доставок: " + totalDeliverySum + " руб.");
+            if (list.equals(orderAddress)){
+            } else {
+                totalAddress ++; 
+            }
+            System.out.print("Общее количество городов: "+ totalAddress + "городов");
             return true;
         } else {
             System.out.println("Доставки по этому адресу нет (" + orderAddress + ").");
