013268d00854b2fc4c0a8105c409b4ef858a2e3c
ivanovroman4999-pixel <ivanovroman4999@gmail.com>
2026-01-11 17:22:40 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,3 +1,8 @@
+import java.util.*;
+import java.util.stream.Stream;
+
+import static java.util.Locale.filter;
+
 public class Main {
     public static void main(String[] args) {
         Calculator calc = Calculator.instance.get();
@@ -5,6 +10,20 @@ public class Main {
         int b = calc.minus.apply(1,1);
         int c = calc.devide.apply(a, b); // тут происходит ошибка при делении на 0
         calc.println.accept(c);
+
+        OnTaskDoneListener listener = System.out::println;
+        Worker worker = new Worker(listener);
+        worker.start();
+
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        Collections.sort(intList);
+        List<Integer> result = new ArrayList<>();
+        for(int i : intList){
+            if (i > 0 && i%2 == 0){
+                result.add(i);
+                System.out.println(i);
+            }
+        }
     }
 }
 
